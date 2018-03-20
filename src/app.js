304c0bbb5e322a3ea691714d0bd87cf7b85438fb
Assaf Sapir <meijin007@gmail.com>
2018-03-20 22:58:27 +0200

    Move to fully async

--- src/app.js
+++ src/app.js
@@ -1,24 +1,29 @@
 'use strict';
 
-function getTokens() {
+async function getTokens() {
     const fs = require('fs');
+    const promisify = require('util').promisify;
+    const readFile = promisify(fs.readFile);
     try {
-        const content = fs.readFileSync('cred.json', 'utf8');
+        const content = await readFile('cred.json', 'utf8');
         return JSON.parse(content);
     } catch (error) {
         console.log(error);
     }
 }
 
-const Twit = require('twit');
-const twit_instance = new Twit(getTokens());
+async function getResult(params) {
+    const Twit = require('twit');
+    const twit_instance = new Twit(await getTokens());
+    return await twit_instance.get('statuses/user_timeline', params);
+}
 
 async function getTweets(params) {
-    let result = await twit_instance.get('statuses/user_timeline', params);
     try {
-        if (result.resp.statusCode !== 200) 
+        const result = await getResult(params);
+        if (result.resp.statusCode !== 200)
             throw new Error(`Well... ${result.resp.statusMessage}`);
-        
+
         let mostFavTweet = 0;
         let maxFav = 0;
         result.data.forEach(element => {
@@ -31,13 +36,12 @@ async function getTweets(params) {
     } catch (error) {
         console.log(error);
     }
-
 }
 
-const firstParams = {
+const params = {
     screen_name: 'meijin007',
     count: 200,
     exclude_replies: true
 };
 
-getTweets(firstParams);
+getTweets(params);
