a7b254ad8a42a11d987e765204780448539a33a2
Assaf Sapir <meijin007@gmail.com>
2018-03-25 22:36:57 +0300

    add more tests

--- test/app-tests.js
+++ test/app-tests.js
@@ -1,13 +1,25 @@
 'use strict';
 
-const expect   = require(`chai`).expect,
-      app = require(`../lib/app`).default;
+const sinon = require(`sinon`),
+    expect = require(`chai`).expect,
+    app = require(`../lib/app`).default;
 
 describe(`Module export`, () => {
     it(`Things are exported`, () => {
-        expect(app).to.be.a(`object`);
+        expect(app).to.be.an(`object`);
         expect(app.getTweets).to.be.a(`function`);
-        expect(app.defaultParams).to.be.a(`object`);
+        expect(app.defaultParams).to.be.an(`object`);
+    });
+    it(`Not responed to unexported function`, () => {
+        expect(app).itself.to.respondTo(`getTweets`).
+            but.not.respondTo(`getTokens`).
+            and.not.respondTo(`getResult`);
     });
 });
 
+describe(`getTweets function`, () => {
+    it(`Have default parameters`, () => {
+        sinon.mock(`getResult`).withExactArgs(app.defaultParams);
+        app.getTweets();
+    });
+});
